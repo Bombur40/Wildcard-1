0552da9852f84086aeba92a21a931afe946b8a5c
Jacob Sommer <jacob.sommer@comcast.net>
2021-11-03 00:27:24 -0700

    Add item template

--- Assets/Item.cs
+++ Assets/Item.cs
@@ -1,5 +1,17 @@
 using UnityEngine;
 
-public abstract class Item : MonoBehaviour {
+public enum Rarity {
+    Common = 0,
+    Uncommon = 1,
+    Rare = 2
+}
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Item")]
+public class Item : ScriptableObject {
     
+    public new string name;
+    public string description;
+    public Sprite texture;
+    public Rarity rarity;
+
 }
