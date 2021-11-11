05e8987bd4b4a6bc82cfac294f9f1fe54de75c33
Bombur40 <56006579+Bombur40@users.noreply.github.com>
2021-11-10 18:55:45 -0800

    Pulling William's Pot

---
+++ Assets/BigBoil.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigBoil : MonoBehaviour
+{
+    [SerializeField] private GameObject potPrefab;
+    [SerializeField] private int health = 100;
+    [SerializeField] private float cooldown = 5;
+    
+    //Creates a clone of the pot
+    private void Spawn(Vector2 mousePos) {
+        GameObject pot = Instantiate(potPrefab) as GameObject;
+        Pot p = pot.GetComponent<Pot>();
+        p.SetHealth(health);
+
+        pot.transform.position = mousePos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Spawn(mousePos);
+        }
+    }
+}
